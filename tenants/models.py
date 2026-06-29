2b8a2a6c434a51ff50de37283f12341db7e59bcf
mbrk <alimoubarak2000fr@gmail.com>
2026-06-29 20:53:17 +0100

    Create lease app

--- tenants/models.py
+++ tenants/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Tenant(models.Model):
     full_name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=15)
+    entry_date = models.DateField()
+    residence_number = models.CharField(max_length=10)
     active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
